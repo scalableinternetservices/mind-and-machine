24fead21a189c94ac40faad03b83086f8735a893
Parsa Madinei <madinei@ucsb.edu>
2024-11-10 23:16:11 -0800

    Final Project Foundation Commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
+  
+  def new
+  end
+  
+  def create
+    user = User.find_or_create_by(username: params[:username])
+    session[:user_id] = user.id
+    redirect_to root_path
+  end
+  
+  def destroy
+    session[:user_id] = nil
+    redirect_to login_path
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  root 'posts#index'
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+  
+  resources :posts do
+    resources :comments, only: [:create]
+  end
+end
