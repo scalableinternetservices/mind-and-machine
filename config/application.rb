a22eabc5c5bc7310f61b97c00b21930234c2e4e5
YoungHypo <haiboyang@smail.nju.edu.cn>
2024-11-17 16:53:39 -0800

    change config dir to proj3

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module MindAndMachine
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    config.action_view.embed_authenticity_token_in_remote_forms = true
 
     # Configuration for the application, engines, and railties goes here.
     #
