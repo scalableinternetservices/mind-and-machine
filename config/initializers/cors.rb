4bc797284e035421226ee95c9a2e5ad5be110c72
YoungHypo <haiboyang@smail.nju.edu.cn>
2024-12-06 15:56:19 -0800

    delete new source for posts and comments in cors for guest mode

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,18 +2,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:8000',
             /\Ahttp:\/\/.*\.elasticbeanstalk\.com\z/
-          
-    resource '/api/posts',
-      headers: :any,
-      methods: [:get, :post],
-      credentials: false
 
-    resource '/api/comments',
-      headers: :any,
-      methods: [:get, :post],
-      credentials: false
-
-    resource '/api/*',
+    resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
